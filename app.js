7127f7fc738ad9e70138ed7a59d43e5e24b0f82b
Jack <mjack6182@gmail.com>
2025-02-01 22:05:05 -0600

    changed up the css to fix the projects view on moblie, also changed it so not as many particles showed up on moblie

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 // Initialize Particles.js
 document.addEventListener('DOMContentLoaded', () => {
+    let screenWidth = window.innerWidth;
+
+    let particleCount = screenWidth > 768 ? 100 : 50;
+
     var particles = Particles.init({
         selector: '.background', // Target the container
         color: '#FF0000',        // Set particle color
         connectParticles: true,  // Connect lines between particles
-        maxParticles: 100        // Limit the number of particles
+        maxParticles: particleCount        // Limit the number of particles
     });
 
     console.log('Particles.js initialized successfully!');
